c9af6b59f3598bf2be74f50d0baa34240034ed05
soderc <christoffer.soderberg@ons.gov.uk>
2025-06-26 13:58:56 +0100

    Make estimation placeholder explicit

--- R/main.R
+++ R/main.R
@@ -1,10 +1,10 @@
 rm(list = ls())
 # Load the functions
-source("config.R")
-source("input_functions.R")
-source("pre_processing.R")
-source("regenesses_estimation.R")
-source("export_results.R")
+source("R/config.R")
+source("R/input_functions.R")
+source("R/pre_processing.R")
+source("R/regenesses_estimation.R")
+source("R/export_results.R")
 
 
 # Main Function to run the project
@@ -17,14 +17,24 @@ main <- function(input_data_path, population_counts_path){
   # pre-process the input data
   input_data_with_counts <- pre_process_data(input_data, population_counts)
 
-  # estimation -> estimates$total_estimates, estimates$size_band_estimates
-  estimates <- regenesses_estimation(input_data_with_counts)
+  # todo: Placeholder for estimation
+  estimates <- list(
+    total_estimates = data.frame(
+      winsorised_value = input_data_with_counts$winsorised_value,
+      size_band = input_data_with_counts$size_band,
+      question_no = input_data_with_counts$question_no
+    ),
+    size_band_estimates = data.frame(
+      size_band = input_data_with_counts$size_band,
+      question_no = input_data_with_counts$question_no
+    )
+  )
 
   export_results(
     input_data_with_counts,
     estimates,
     PROCESSED_DATA_FILE
-    )
+  )
 }
 
 # call the main function
