a3e8e2ef724709580f8f53df42f5e570b83c340b
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2025-07-22 15:16:55 +0100

    1060 reader wrapper for s3 (#11)
    * Add utils functions for hdfs commands
    
    * Add input output wrappers for s3
    
    * Apply wrappers to main
    
    * Remove export_results module, function now in io.R
    
    * Fix error in stop message
    
    * Fix formating issues with functions
    
    * Add storage system arg to write wrapper
    
    * Move checking storage arg to its own function
    
    * Remove test-export testing since no module
    
    * Add return to check function
    
    * Add description to function arg
    
    * Fix path issues for s3
    
    * Fix unused args
    
    * Apply changes for s3 (tested)
    
    * Reupload namespace
    
    * Update function docs, add export too
    
    * Fix errors, warnings via dev check
    
    * Replace stop process with messages

--- R/main.R
+++ R/main.R
@@ -1,15 +1,18 @@
 #' Main Function to run the project
 #'
+#' @param storage_system accepts local or s3
 #' @param input_data_path file path of main dataframe
 #' @param population_counts_path file path of population counts
 #' @param output_path file path to save results
 #'
 #' @export
-main <- function(input_data_path, population_counts_path, output_path){
+main <- function(storage_system,input_data_path, population_counts_path, output_path){
+
+  check_storage_system_arg(storage_system)
 
   # load the input data
-  input_data <- read.csv(input_data_path)
-  population_counts <- read.csv(population_counts_path)
+  input_data <-read_csv_wrapper(storage_system,input_data_path)
+  population_counts <- read_csv_wrapper(storage_system,population_counts_path)
 
   # pre-process the input data
   input_data_with_counts <- pre_process_data(input_data, population_counts)
@@ -33,16 +36,16 @@ main <- function(input_data_path, population_counts_path, output_path){
 
   print("Merging estimates to source dataframe")
 
-  #ToDo:  function extract file name from snapshot
 
-  out_file_name <- paste0(output_path,"regenesses_extracts.csv")
+  output_df <- merge(
+  input_data_with_counts,
+  estimates,
+  by = c("period", "questioncode")
+)
 
-  export_results(
-    input_data_with_counts,
-    estimates,
-    out_file_name
-  )
+  file_name <- create_rsurveymethods_file_name(input_data_path)
 
+  write_csv_wrapper(output_df,storage_system,output_path,file_name)
   print("Process was succesful")
 }
 
