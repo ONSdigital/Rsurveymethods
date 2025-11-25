cbf5564353f67a6024d1f26d09ddea8877b048de
Jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-11-25 08:02:37 +0000

    Update R/utils.R
    Co-authored-by: NathanKelly-ONS <93586797+NathanKelly-ONS@users.noreply.github.com>

--- R/utils.R
+++ R/utils.R
@@ -133,7 +133,7 @@ format_se_for_publication <- function(df, selected_period=""){
 #' @param platform storage platform, either "s3" or "network"
 #' @return formatted file name
 #' @export
-format_file_name <- function(config, path, file_name, platform) {
+format_file_name <- function(config, path, file_name, platform = c("s3", "network")) {
   formatted_path <- format_path(config, path, platform)
   formatted_file_name <- paste0(formatted_path, file_name, "_", config$run_id, ".csv")
   return(formatted_file_name)
